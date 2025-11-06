5a7e16bb4d14fe7030dfdbd01d8a5f14ed8f8071
Jk <SquareLiquor@gmail.com>
2025-11-06 02:35:11 +0000

    페이지 접근시 store 처리

--- src/routes/(authenticated)/(consumer)/+layout.server.ts
+++ src/routes/(authenticated)/(consumer)/+layout.server.ts
@@ -1,12 +1,10 @@
 import type { LayoutServerLoad } from './$types'
 
 export const load: LayoutServerLoad = async ({ url, cookies, locals: { supabase } }) => {
-  let storeId = url.searchParams.get('s') || cookies.get('store_id')
+  let storeId = cookies.get('store_id')
+  console.log('storeId in coop > layout.server.ts:', storeId)
   if (!storeId) return {}
 
   const { data: store } = await supabase.from('stores').select('*').eq('id', storeId).maybeSingle()
-  if (store) {
-    cookies.set('store_id', storeId, { path: '/', httpOnly: true })
-    return { store }
-  }
+  if (store) return { store }
 }
--- src/routes/+layout.server.ts
+++ src/routes/+layout.server.ts
@@ -1,4 +1,4 @@
-export async function load({ locals: { safeGetSession }, cookies }) {
+export async function load({ cookies, locals: { safeGetSession } }) {
   /**
    * TODO: warning 발생 원인 해결 필요, safeGetSession 에서 getSession 호출로 인해 발생
    * Using the user object as returned from supabase.auth.getSession() or from some supabase.auth.onAuthStateChange() events could be insecure! This value comes directly from the storage medium (usually cookies on the server) and may not be authentic. Use supabase.auth.getUser() instead which authenticates the data by contacting the Supabase Auth server
